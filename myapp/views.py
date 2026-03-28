1a149483c46dd623b4cdcde28bda46d890a305bc
jenisbaevna <jumanazarovaa05@gmail.com>
2026-03-28 23:14:56 +0500

    detaik ham design ozgerdi

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
 from .models import Students,Phones
 # Create your views here.
 
@@ -10,3 +10,9 @@ def homepage(request):
         'telefonlar':phones
     }
     return render(request,'home.html',context=context)
+def detail(request,id):
+    telefon=get_object_or_404(Phones,id=id)
+    context={
+        'telefon':telefon
+    }
+    return render(request,'detail.html',context=context)
